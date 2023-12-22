43032c784dbfb9e3b1dc0bd083977553e5f3bddc
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2023-12-22 10:03:24 +0100

    dansende sfære af Tanja tilføjet

--- scripts/sites.js
+++ scripts/sites.js
@@ -1,38 +1,44 @@
 export const sites = [
-  /* {
+	/* {
     name: ---------- HER SKAL DIN TITEL VÆRE----------
     text: ---------- HER SKAL DIN TEKST VÆRE----------,
     js_file_name: ---------- HER SKAL DIN P5 FIL VÆRE ----------,
     og_img_file_name:  ---------- HER SKAL DIT ORIGNALE BILLEDE VÆRE----------,
     mod_img_file_name:  ---------- HER SKAL DIT BILLEDE AF KUNSTVÆRKET VÆRE ----------,
   }, */
-  {
-
-    name: "Gadens Øje",
-    text: "Gadens Øje er et kunstværk, der træder ind i byrummet som en visuel fortælling og symboliserer et unikt perspektiv på det urbane miljø. Dette imponerende kunstværk udfolder sig på en stor murflade og fremstår som et enestående og nærmest magisk indslag i byens struktur.Værket består af et gigantisk øje, der er omhyggeligt udført med levende farver og realistiske detaljer. Øjet fungerer som et metaforisk vindue ind i sjælen af byen, idet det observerer og registrerer alt, der sker omkring det. Den kunstneriske udførelse af iris og pupille kan variere i farvespil, hvilket tilføjer dybde og kompleksitet til værket.",
-    js_file_name: "urban_serenity.js",
-    og_img_file_name: "urban_serenity_original.jpg",
-    mod_img_file_name: "urban_serenity_art.PNG",
-  },
-  {
-    name: "Måneskinnet",
-    text: "Værket måneskinnet tager udgangspunkt i favekombinationen og kompositionen, i det originale værk stjernetåger af Else Alfelt. Kunstværket er lavet ved at udnytte Perlin Noise algoritmen, der genererer semi tilfældige streger og farverne på trådene der trækkes over den sorte nattehimmel er taget fra pixel positionen på det originale værk. Det vil sige at hver gang du tager et kig på måneskinnet vil det tage ny form.",
-    js_file_name: "moonshine.js",
-    og_img_file_name: "moonshine_original.png",
-    mod_img_file_name: "moonshine_art.PNG",
-  },
-  {
-    name: "Breaths",
-    text: "Sophie Dupont, 429 Breaths \n \n Værken '429 Breaths' er en del af serien 'Spatula Breath Paintings', der udtrykker en abstrakt udfoldelse af kunstnerens egen vejrtrækning, idet en dyb indånding går forud for hvert penselstrøg.",
-    js_file_name: "Breaths.js",
-    og_img_file_name: "Breaths_original.jpg",
-    mod_img_file_name: "Breaths_copy.jpg",
-  },
-  {
-    name: "Lysgrafik",
-    text: "Koldt og varmt reflekslys",
-    js_file_name: "lysgrafik.js",
-    og_img_file_name: "lysgrafik_original.jpg",
-    mod_img_file_name: "lysgrafik.PNG",
-  },
+	{
+		name: "Gadens Øje",
+		text: "Gadens Øje er et kunstværk, der træder ind i byrummet som en visuel fortælling og symboliserer et unikt perspektiv på det urbane miljø. Dette imponerende kunstværk udfolder sig på en stor murflade og fremstår som et enestående og nærmest magisk indslag i byens struktur.Værket består af et gigantisk øje, der er omhyggeligt udført med levende farver og realistiske detaljer. Øjet fungerer som et metaforisk vindue ind i sjælen af byen, idet det observerer og registrerer alt, der sker omkring det. Den kunstneriske udførelse af iris og pupille kan variere i farvespil, hvilket tilføjer dybde og kompleksitet til værket.",
+		js_file_name: "urban_serenity.js",
+		og_img_file_name: "urban_serenity_original.jpg",
+		mod_img_file_name: "urban_serenity_art.PNG",
+	},
+	{
+		name: "Måneskinnet",
+		text: "Værket måneskinnet tager udgangspunkt i favekombinationen og kompositionen, i det originale værk stjernetåger af Else Alfelt. Kunstværket er lavet ved at udnytte Perlin Noise algoritmen, der genererer semi tilfældige streger og farverne på trådene der trækkes over den sorte nattehimmel er taget fra pixel positionen på det originale værk. Det vil sige at hver gang du tager et kig på måneskinnet vil det tage ny form.",
+		js_file_name: "moonshine.js",
+		og_img_file_name: "moonshine_original.png",
+		mod_img_file_name: "moonshine_art.PNG",
+	},
+	{
+		name: "Breaths",
+		text: "Sophie Dupont, 429 Breaths \n \n Værken '429 Breaths' er en del af serien 'Spatula Breath Paintings', der udtrykker en abstrakt udfoldelse af kunstnerens egen vejrtrækning, idet en dyb indånding går forud for hvert penselstrøg.",
+		js_file_name: "Breaths.js",
+		og_img_file_name: "Breaths_original.jpg",
+		mod_img_file_name: "Breaths_copy.jpg",
+	},
+	{
+		name: "Lysgrafik",
+		text: "Koldt og varmt reflekslys",
+		js_file_name: "lysgrafik.js",
+		og_img_file_name: "lysgrafik_original.jpg",
+		mod_img_file_name: "lysgrafik.PNG",
+	},
+	{
+		name: "Dansende Sfære",
+		text: "Dansende Sfære er en digital kunstoplevelse, hvor farverige figurer pulserer i dynamisk samspil. Spontane firkanter væver sig mellem strukturerede trapezer og cirkler, mens en levende, grøn sfære udforsker det digitale rum med en konstant bevægelse. Fanlights og fandarks tilføjer kompleksitet, og farver som orange og rød skaber en visuel symfoni. Dette kunstværk indfanger opmærksomheden og inviterer til en unik interaktiv oplevelse, der transcenderer det traditionelle og fejrer den digitale æra.",
+		js_file_name: "dansende_sfære.js",
+		og_img_file_name: "dansende_sfære_copy.png",
+		mod_img_file_name: "dansende_sfære_copy.png",
+	},
 ];
