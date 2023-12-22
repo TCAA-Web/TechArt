305193dc432a496e035ca07141c48b672bcc21ad
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-12-22 06:59:11 +0100

    upd

--- scripts/art/Indo.js
+++ scripts/art/Indo.js
@@ -3,7 +3,7 @@ let img;
 let particles = [];
 
 const PARTICLE_SIZE = 15;
-const RESOLUTION = 20;
+const RESOLUTION = 25;
 const easing = 0.01;
 
 function preload() {
@@ -11,9 +11,11 @@ function preload() {
 }
 
 function setup() {
+    // Create a canvas with WEBGL renderer
     let c = createCanvas(900, 600, WEBGL);
     c.parent("canvasContainer");
 
+    // Enable orbit control for easy 3D navigation if needed(zoom, orbit rundt om model..)
     orbitControl();
 
     spawnParticles();
@@ -23,43 +25,61 @@ function draw() {
     background(0);
     // rotateY(frameCount * 0.01);
     // image(img, 0, 0);
+    // Update and draw each particle
     particles.forEach((particle) => {
         particle.update();
         particle.draw();
     });
 }
 
+// Function to spawn particles based on image pixels
 function spawnParticles() {
     for (let i = 0; i < width; i += RESOLUTION) {
         for (let j = 0; j < height; j += RESOLUTION) {
+            // Map particle positions to image coordinates
             let x = i / width * img.width
             let y = j / height * img.height
 
+            // Get color from image at current position
             const color = img.get(x, y);
-            particles.push(new Particle(i + PARTICLE_SIZE / 2, j + PARTICLE_SIZE / 2, color));
+
+            // Center the particles in the grid
+            let xOffset = (img.width * (RESOLUTION / width)) / 5;
+            let yOffset = (img.height * (RESOLUTION / height)) / 5;
+
+            // Create a new particle with the calculated position and color
+            particles.push(new Particle(i + xOffset + PARTICLE_SIZE / 2, j + yOffset + PARTICLE_SIZE / 2, color));
         }
     }
 }
 
 class Particle {
     constructor(x, y, color) {
+        // Set initial position relative to canvas center
         this.x = x - width / 2;
         this.y = y - height / 2;
         this.z = 0;
+
+        // Store the color of the particle
         this.color = color;
-        this.originalX = this.x; // Store the original positions
+
+        // Store the original positions for smooth transitions
+        this.originalX = this.x;
         this.originalY = this.y;
     }
 
     update() {
-        // Example: Change particle color when the mouse is clicked
+        
         if (mouseIsPressed) {
-            // Example: Move particles away from the mouse cursor
+            // Move particles away from the mouse cursor
             let distanceToMouse = dist(this.x, this.y, mouseX - width / 2, mouseY - height / 2);
             if (distanceToMouse < 50) {
+                // Calculate force vector away from the mouse
                 let force = createVector(this.x - mouseX + width / 2, this.y - mouseY + height / 2);
                 force.normalize();
                 force.mult(10);
+
+                // Apply frce to particle position
                 this.x += force.x;
                 this.y += force.y;
             }        
@@ -72,16 +92,20 @@ class Particle {
 
     draw() {
         fill(this.color);
+        // Disable stroke for a cleaner look
         noStroke();
+        // Push and translate to the particle's position
         push();
         translate(this.x, this.y, this.z);
 
+        // Create a box at the particle's position with rotation for visual effects
         for (let i = 0; i < 4; i++) {
-            rotateY(HALF_PI);
+            rotateY(frameCount * 0.01);
             rotateX(frameCount * 0.01);
             box(PARTICLE_SIZE);
         }
 
+        // Pop to revert transformation
         pop();
     }
 }
